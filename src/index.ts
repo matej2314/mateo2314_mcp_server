5c0e20fc75970ecb25b7ec4d3bdbd83616121df6
mateo2314 <mateo2314@gmail.com>
2026-04-17 22:10:02 +0200

    Fixed env file path

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
 
 import { createMcpServer } from './core/server.js';
 import type { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
